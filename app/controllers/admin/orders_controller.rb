da3fb92e995ab1caff32925c87ba3d0999d632df
kakeru0329 <109051653+kakeru0329@users.noreply.github.com>
2022-09-03 12:17:20 +0000

    履歴詳細中身入れ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,14 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
   end
+  
+  
+  
+  private
+  def item_params
+    params.require(:order).permit(:customer_id, :address, :postal_code, :totle_amount,
+    :freight, :payment_method, :name)
+  end
+
 end
